0de60030aac6d487dae47cd1fced5c9c24d068ab
schrodinger guru <67430518+ChowdaryManyam@users.noreply.github.com>
2024-11-13 00:17:42 +0100

    Added RGB-Control project files and removed PWM-Tone files

---
+++ Examples/RGB-Control/obj/Peripheral/src/ch32v00x_gpio.d
@@ -0,0 +1,70 @@
+Peripheral/src/ch32v00x_gpio.o: ../Peripheral/src/ch32v00x_gpio.c \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_gpio.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Core/core_riscv.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/system_ch32v00x.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_conf.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_adc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dbgmcu.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dma.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_exti.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_flash.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_i2c.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_it.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Debug/debug.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_iwdg.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_misc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_pwr.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_rcc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_spi.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_tim.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_usart.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_wwdg.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_opa.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_gpio.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Core/core_riscv.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/system_ch32v00x.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_conf.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_adc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dbgmcu.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dma.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_exti.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_flash.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_i2c.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_it.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Debug/debug.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_iwdg.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_misc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_pwr.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_rcc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_spi.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_tim.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_usart.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_wwdg.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_opa.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h:
---
+++ Examples/RGB-Control/obj/User/main.d
@@ -0,0 +1,77 @@
+User/main.o: ../User/main.c \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Debug/debug.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Core/core_riscv.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/system_ch32v00x.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_conf.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_adc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dbgmcu.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dma.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_exti.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_flash.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_gpio.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_i2c.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_it.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_iwdg.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_misc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_pwr.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_rcc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_spi.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_tim.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_usart.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_wwdg.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_opa.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h \
+ ../User/lookups.h ../User/GD_WS2812_DRIVER.h ../User/LED_functions.h
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Debug/debug.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Core/core_riscv.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/system_ch32v00x.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_conf.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_adc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dbgmcu.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_dma.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_exti.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_flash.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_gpio.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_i2c.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/User/ch32v00x_it.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_iwdg.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_misc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_pwr.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_rcc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_spi.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_tim.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_usart.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_wwdg.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x_opa.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-03_RGB-LED-Control/RGB-Control/Peripheral/inc/ch32v00x.h:
+
+../User/lookups.h:
+
+../User/GD_WS2812_DRIVER.h:
+
+../User/LED_functions.h:
