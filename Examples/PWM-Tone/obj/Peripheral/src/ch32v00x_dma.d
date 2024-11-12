ef1b9bfec1d3166bee78569a7319c9d05f24f744
schrodinger guru <67430518+ChowdaryManyam@users.noreply.github.com>
2024-11-13 00:38:30 +0100

    Added PWM-Tone project files

---
+++ Examples/PWM-Tone/obj/Peripheral/src/ch32v00x_dma.d
@@ -0,0 +1,70 @@
+Peripheral/src/ch32v00x_dma.o: ../Peripheral/src/ch32v00x_dma.c \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_dma.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Core/core_riscv.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/User/system_ch32v00x.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/User/ch32v00x_conf.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_adc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_dbgmcu.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_exti.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_flash.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_gpio.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_i2c.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/User/ch32v00x_it.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Debug/debug.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_iwdg.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_misc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_pwr.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_rcc.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_spi.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_tim.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_usart.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_wwdg.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_opa.h \
+ /home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x.h
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_dma.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Core/core_riscv.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/User/system_ch32v00x.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/User/ch32v00x_conf.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_adc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_dbgmcu.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_exti.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_flash.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_gpio.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_i2c.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/User/ch32v00x_it.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Debug/debug.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_iwdg.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_misc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_pwr.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_rcc.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_spi.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_tim.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_usart.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_wwdg.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x_opa.h:
+
+/home/chowdarymanyam/Downloads/Work-Space/Firmware/CH32V/27082024_MounRiver/Workspace/SecURad/Task-04_PWM-Tone-Generation/PWM-Tone/Peripheral/inc/ch32v00x.h:
